38125ac211c52edef5ed61bb866e4018cdb97e7b
Robin Daugherty <robin@robindaugherty.net>
2022-03-30 16:12:55 -0400

    Fix rbenv and nodenv for deployment users

--- roles/ruby_app/templates/app-env.sh
+++ roles/ruby_app/templates/app-env.sh
@@ -1,5 +1,10 @@
 {% for key, value in app.environment.items() %}
-  export {{key}}="{{value}}"
+export {{key}}="{{value}}"
 {% endfor %}
 export TZ=UTC
 export APP_NAME={{app.name}}
+
+. /etc/profile.d/rbenv.sh
+{% if node_version is defined %}
+. /etc/profile.d/nodenv.sh
+{% endif %}
